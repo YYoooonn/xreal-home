e42423d0627a4891223566ca12e1ded597674589
Sharlotte <seeseafood@naver.com>
2023-07-25 22:20:47 +0900

    feat: add utils/delay

---
+++ src/utils/delay.ts
@@ -0,0 +1,7 @@
+/**
+ * async / await promise delay function
+ * @param {number} delay miliseconds 단위의 딜레이 시간
+ * @returns {Promise<void>} 비동기 함수. 반환 없음
+ */
+export default (ms: number): Promise<void> =>
+  new Promise((res) => setTimeout(res, ms));
